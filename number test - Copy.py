67767c5da41cf3e7ffa83dfd1fa650966d7c1358
MonishaCode <monishasultana07@gmail.com>
2019-10-28 19:07:19 +0530

    commitment3

---
+++ number test - Copy.py
@@ -0,0 +1,14 @@
+# A number even or odd
+
+num=int(input())
+mod=num%2
+if mod > 0:
+    print('This is an odd number')
+elif mod <= 0:
+    print('This is an even number')
+else:
+    print('This is not an even and not an odd number')
+    
+
+   
+    
