fa0362b58db7bd83046a7bbbc10888922ca6fe43
MonishaCode <monishasultana07@gmail.com>
2019-10-28 18:30:01 +0530

    commitment 2

---
+++ number.py
@@ -0,0 +1,49 @@
+a=int(input())
+b=int(input())
+sum=a+b
+dif=a-b
+mul=a*b
+if b==0:
+  print('Error')
+  return 0
+div=a//b
+Exp=a**b
+print('sum of a and b is:',sum)
+print('dif of a and b is:',dif)
+print('mul of a and b is:',mul)
+print('div of a and b is:',div)
+print('Exp of a and b is:',Exp)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
